4710fdf640a605c754b36a9ea94ce74dc1bedd4d
harivarshini22 <harivarshini228@gmail.com>
2026-08-19 15:21:23 +0530

    enum

--- Oops/Main.java
+++ Oops/Main.java
@@ -11,3 +11,19 @@ public Order(int orderId,String orderStatus){
     this.orderId=orderId;
     this.orderStatus=orderStatus;
 }
+enum OrderStatus{
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
+public class Main{
+    public static void main(String[] args){
+        Order order=new Order(orderId:1);
+        System.out.println("Order ID: " + order.orderId);
+        Order order1 = new Order(orderId:2, OrderStatus,PENDING);
+        Order order2=new Order(orderID:2, OrderStatus.PENDING);
+        System.out.prinln("Order status:"+ order.orderStatus);
+
+    }
+}

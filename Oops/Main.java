e7601e016dc003025d80821e316e149ce7ebf9f5
harivarshini22 <harivarshini228@gmail.com>
2026-08-19 15:40:54 +0530

    access modifier

--- Oops/Main.java
+++ Oops/Main.java
@@ -1,6 +1,11 @@
 class Order{
     public int orderId;
     public String orderStatus;
+    private double orderPrice;
+    public Order(int orderId,OrderStatus order)
+        this.orderId=orderId;
+        this.orderStatus=orderStatus;
+        this.orderPrice=orderPrice;
 }
 
 public Order(int orderID){
@@ -24,6 +29,8 @@ public class Main{
         Order order1 = new Order(orderId:2, OrderStatus,PENDING);
         Order order2=new Order(orderID:2, OrderStatus.PENDING);
         System.out.prinln("Order status:"+ order.orderStatus);
+        Order order 3=new Order(3,OrderStatus.SHIPPED,999);
+        System.out.println("Order price: ",+order3.getOrderPrice());
 
     }
 }
